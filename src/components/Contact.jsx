9b10dff87e0eb2d0c1a3fc2e4d490fbbe503e136
krsanthiya2003-ux <krsanthiya2003@gmail.com>
2025-12-28 20:15:54 -0500

    welcome

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
 
-    const Contact = () => {
-      return (
+const Contact = () => {
+  return (
     <div style={styles.container}>
       <h1>aktsupermarket2003@gmail.com</h1>
       <p>Thillai Nagar,Trichy .</p>
-    </div>
-  );
-};
+  </div>
+  )
+}
 
-const styles = {
-  container: {
-    textAlign: "center",
-    padding: "50px",
-    fontFamily: "Arial, sans-serif",
-  },
-};
-
-export default Contact;
- 
+export default Contact
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,9 @@
-
-import React from "react";
+import React from 'react'
 
 const Home = () => {
   return (
-    <div style={styles.container}>
-      <h1>A.K.T Super Market</h1>
-      <h2>Welcome to All!</h2>
-      <p>Buy one Get One Offer!.</p>
-    </div>
-  );
-};
+    <div>Home</div>
+  )
+}
 
-const styles = {
-  container: {
-    textAlign: "center",
-    padding: "50px",
-    fontFamily: "Arial, sans-serif",
-  },
-};
-
-export default Home;
+export default Home
